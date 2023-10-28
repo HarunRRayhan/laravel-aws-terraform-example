df8d6f3581438d3eb2015c1b34f86f0d57afc252
Harun R. Rayhan <harun.b13@gmail.com>
2023-10-28 19:22:15 +0600

    Backend & Variables

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+    description = "Choose your default AWS region..."
+}
+
+variable "app_name" {
+    type = string
+    description = "Choose your application name..."
+}
